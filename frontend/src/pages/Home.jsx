996d0fd608cd48b0d8f0b1eec002001405baaa89
ilhan <ilhan.balci.10@hotmail.com>
2024-03-06 12:05:40 +0300

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,6 @@
-import currentEventBanner from '../images/pgl_copenhagen_major_2024_banner.webp'
-
 export default function Home() {
     return (
         <div className="home-page container">
-            <div className='current-event-banner-wrapper'>
-                <h1>PGL Copehagen Major 2024</h1>
-                <img className='current-event-banner' src={currentEventBanner}/>
-            </div>
         </div>
     )
 }
